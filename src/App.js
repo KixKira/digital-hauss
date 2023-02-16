52214795437fbed051f776a6b75a7b8f76efa574
Oscar Machado <oemqkix@gmail.com>
2023-02-16 19:10:26 -0400

    Creation of footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import Hero from './components/UI/Hero/Hero';
 import About from './components/UI/About/About';
 import Services from './components/UI/Services/Services';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Services />
+      <Footer />
     </>
   );
 }
